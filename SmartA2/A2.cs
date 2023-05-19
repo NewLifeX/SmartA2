99a80f9328d3284727069365cb9b8d9067ea0999
大石头 <nnhy@vip.qq.com>
2023-05-19 15:27:12 +0800

    修正串口序号

--- SmartA2/A2.cs
+++ SmartA2/A2.cs
@@ -43,7 +43,7 @@ public class A2
     {
         if (com <= 0 || com > 4) throw new ArgumentOutOfRangeException(nameof(com), $"无效串口COM{com}，支持COM1/COM2/COM3/COM4");
 
-        return new SerialPort(ComNames[com], baudrate);
+        return new SerialPort(ComNames[com - 1], baudrate);
     }
 
     /// <summary>创建Modbus</summary>
@@ -55,7 +55,7 @@ public class A2
     {
         if (com <= 0 || com > 4) throw new ArgumentOutOfRangeException(nameof(com), $"无效串口COM{com}，支持COM1/COM2/COM3/COM4");
 
-        return new ModbusRtu { PortName = ComNames[com], Baudrate = baudrate };
+        return new ModbusRtu { PortName = ComNames[com - 1], Baudrate = baudrate };
     }
     #endregion
 
@@ -83,7 +83,7 @@ public class A2
             foreach (var line in lines)
             {
                 var ss = line.Split(' ', '\t');
-                if (ss[0] == "127.0.0.1" && ss[ss.Length - 1] == name)
+                if (ss[0] == "127.0.0.1" && ss[^1] == name)
                 {
                     flag = true;
                     break;

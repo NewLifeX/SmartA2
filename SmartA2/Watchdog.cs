8ce51af53574199ea37bd9a37990abf8725026d7
勇强 汪 <W13971612060@outlook.com>
2023-06-06 16:06:29 +0800

    修改 喂狗 方法

--- SmartA2/Watchdog.cs
+++ SmartA2/Watchdog.cs
@@ -1,20 +1,26 @@
 ﻿namespace SmartA2;
 
-/// <summary>看门狗。Open打开后，需要定期Feed喂狗，否则系统重启</summary>
-/// <remarks>
-/// 看门狗的超时时间为 10 秒，如果超过 10 秒没有喂狗，系统将自动重启
-/// </remarks>
+/// <summary>
+/// 看门狗。Open打开后，需要定期Feed喂狗，否则系统重启
+/// </summary>
+/// <remarks>看门狗的超时时间为 10 秒，如果超过 10 秒没有喂狗，系统将自动重启</remarks>
 public class Watchdog
 {
-    /// <summary>文件路径</summary>
+    /// <summary>
+    /// 文件路径
+    /// </summary>
     public String FileName { get; set; }
 
     private FileStream _stream;
 
-    /// <summary>打开看门狗</summary>
+    /// <summary>
+    /// 打开看门狗
+    /// </summary>
     public void Open() => _stream = new FileStream(FileName, FileMode.OpenOrCreate);
 
-    /// <summary>关闭看门狗</summary>
+    /// <summary>
+    /// 关闭看门狗
+    /// </summary>
     public void Close()
     {
         if (_stream == null) return;
@@ -29,13 +35,17 @@ public class Watchdog
         _stream = null;
     }
 
-    /// <summary>喂狗</summary>
+    /// <summary>
+    /// 喂狗
+    /// </summary>
     public void Feed()
     {
-        var buf = new Byte[1];
-        buf[0] = (Byte)'1';
+        //var buf = new Byte[1];
+        //buf[0] = (Byte)'1';
 
-        _stream.Position = 0;
-        _stream.Write(buf, 0, buf.Length);
+        //_stream.Position = 0;
+        //_stream.Write(buf, 0, buf.Length);
+        // 向看门狗设备写入数据，刷新watchdog计数器
+        File.WriteAllText(FileName.GetFullPath(), "1");
     }
 }

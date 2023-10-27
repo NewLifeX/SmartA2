3d2a3a384056dcc63c26d767b82ef220f56a680f
智能大石头 <nnhy@vip.qq.com>
2023-10-27 23:12:31 +0800

    增加Led闪灯例程

---
+++ Samples/LedTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using SmartA2;
+
+var led = new OutputPort { FileName = "/dev/led" };
+
+while (true)
+{
+    led.Write(false);
+
+    Thread.Sleep(500);
+
+    led.Write(true);
+
+    Thread.Sleep(500);
+}

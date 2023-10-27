6f5e787d25604040bd8daf51cb1417ea5f4e3d8c
智能大石头 <nnhy@vip.qq.com>
2023-10-28 00:18:27 +0800

    增加网络客户端与服务端例程

---
+++ Samples/NetClientTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using NewLife;
+using NewLife.Log;
+using NewLife.Net;
+
+// 网络客户端，一般跑在工控机上，充当硬件设备到服务器之间的桥梁
+
+XTrace.UseConsole();
+
+// 支持tcp/udp地址
+XTrace.WriteLine("请输入要连接的服务器：");
+var server = Console.ReadLine();
+if (server.IsNullOrEmpty()) server = "tcp://10.0.2.6:777";
+
+var uri = new NetUri(server);
+var client = uri.CreateRemote();
+client.Log = XTrace.Log;
+client.LogSend = true;
+client.LogReceive = true;
+
+// 在事件中接收数据
+client.Received += (s, e) =>
+{
+    XTrace.WriteLine("收到数据：{0}", e.Packet.ToStr());
+};
+client.Open();
+
+// 发送数据
+for (var i = 0; i < 10; i++)
+{
+    XTrace.WriteLine("请输入要发送的数据：");
+    var input = Console.ReadLine();
+
+    client.Send(input);
+}
---
+++ Samples/NetServerTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using NewLife.Log;
+using NewLife.Net;
+
+// 网络服务端，一般跑在服务器或上位机上，用于接收工控机内客户端的数据
+
+XTrace.UseConsole();
+
+var server = new NetServer(777)
+{
+    Log = XTrace.Log,
+    SessionLog = XTrace.Log
+};
+
+// 新连接会话事件
+server.NewSession += (s, e) =>
+{
+    var uri = e.Session.Remote;
+    XTrace.WriteLine("新会话：{0}", uri);
+
+    var session = e.Session;
+    session.Send($"欢迎：{uri}");
+};
+
+// 在事件中接收数据
+server.Received += (s, e) =>
+{
+    var msg = e.Packet.ToStr();
+    XTrace.WriteLine("收到数据：{0}", msg);
+
+    // 倒序返回
+    var session = s as INetSession;
+    var cs = msg.Reverse().ToArray();
+    session.Send(new String(cs));
+};
+
+server.Start();
+
+// 等待退出
+Console.ReadLine();

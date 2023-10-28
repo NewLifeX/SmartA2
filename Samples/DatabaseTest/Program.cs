ddc2a3c9bd7ce57a396e7e5eb85eb1728b16b6a6
大石头 <nnhy@vip.qq.com>
2023-10-28 23:22:38 +0800

    增加数据库操作

---
+++ Samples/DatabaseTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using NewLife.Log;
+using NewLife.Serialization;
+using XCode.Membership;
+
+XTrace.UseConsole();
+
+// 新增用户插入数据库
+var user = new User
+{
+    Name = "Stone",
+    DisplayName = "大石头",
+    Password = "123456",
+    Enable = true
+};
+user.Insert();
+
+// 查询用户
+var user2 = User.FindByName("Stone");
+XTrace.WriteLine("FindByName: {0}", user2.ToJson(true));
+
+// 更新用户
+user2.DisplayName = "智能大石头";
+user2.Update();
+
+var user3 = User.FindByID(user2.ID);
+XTrace.WriteLine("FindByID: {0}", user3.DisplayName);
+
+// 删除用户
+user3.Delete();

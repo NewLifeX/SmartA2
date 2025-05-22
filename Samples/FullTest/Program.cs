b78954948fd354941ceaf997a3c8e6b682970483
智能大石头 <nnhy@vip.qq.com>
2025-05-22 23:24:42 +0800

    [feat] 新增A2完全测试的例程项目

---
+++ Samples/FullTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using FullTest;
+using NewLife.Log;
+using NewLife.Model;
+using NewLife.MQTT;
+using Stardust;
+
+// 启用控制台日志，拦截所有异常
+XTrace.UseConsole();
+
+// 初始化对象容器，提供依赖注入能力
+var services = ObjectContainer.Current;
+services.AddSingleton(XTrace.Log);
+
+// 配置星尘。自动读取配置文件 config/star.config 中的服务器地址
+var star = services.AddStardust();
+
+// 初始化Redis、MQTT、RocketMQ，注册服务到容器
+InitMqtt(services, star?.Tracer);
+
+// 注册后台任务 IHostedService
+services.AddHostedService<Worker>();
+
+// 异步阻塞，友好退出
+var host = services.BuildHost();
+await host.RunAsync();
+
+static void InitMqtt(IObjectContainer services, ITracer tracer)
+{
+    // 引入 MQTT
+    var mqtt = new MqttClient
+    {
+        Server = "tcp://127.0.0.1:1883",
+        ClientId = Environment.MachineName,
+        UserName = "stone",
+        Password = "Pass@word",
+
+        Log = XTrace.Log,
+        Tracer = tracer,
+    };
+    services.AddSingleton(mqtt);
+}
---
+++ Samples/FullTest/Worker.cs
@@ -0,0 +1,55 @@
+﻿using NewLife;
+using NewLife.Log;
+using NewLife.Model;
+
+namespace FullTest;
+
+/// <summary>
+/// 后台任务。支持构造函数注入服务
+/// </summary>
+public class Worker : IHostedService
+{
+    private NodeClient _client;
+    private readonly ILog _log;
+    private readonly ITracer _tracer;
+
+    public Worker(ILog log, ITracer tracer)
+    {
+        _log = log;
+        _tracer = tracer;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        XTrace.WriteLine("开始Node客户端");
+
+#if DEBUG
+        // 降低日志等级，输出通信详情。生产环境不建议这么做
+        XTrace.Log.Level = LogLevel.Debug;
+#endif
+
+        var set = ClientSetting.Current;
+
+        // 产品编码、产品密钥从IoT管理平台获取，设备编码支持自动注册
+        var client = new NodeClient(set)
+        {
+            ProductCode = "A2FullTest",
+
+            Tracer = _tracer,
+            Log = _log,
+        };
+
+        client.Open();
+
+        _client = client;
+
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _client.TryDispose();
+
+        return Task.CompletedTask;
+    }
+}
